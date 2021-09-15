b98567935181d314af79c053336bbb01794ecb1e
Rodolfo Mariano <rodolfo.mariano@outlook.com>
2021-09-15 09:55:18 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+import styles from '../styles/Login.module.css'
+
+const Login: NextPage = () => {
+  return (
+    <h1>Home</h1>
+  )
+}
+
+export default Login
