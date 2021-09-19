4e053abba7906e63fda3b43b971f5f8951bdc4d5
Rodolfo Mariano <rodolfo.mariano@outlook.com>
2021-09-18 21:17:43 -0300

    Create upload function

--- src/pages/app/Home/index.tsx
+++ src/pages/app/Home/index.tsx
@@ -1,6 +1,8 @@
-import { useEffect } from 'react'
-import { useSession, signIn, signOut } from "next-auth/client"
+import { ChangeEvent, useEffect, useState } from 'react'
+import { useSession } from "next-auth/client"
 import { useRouter } from 'next/dist/client/router'
+
+import firebase from '../../../services/firebase'
 
 import { TopBar } from '../../../components/TopBar'
 import { NavBar } from '../../../components/NavBar'
@@ -9,12 +11,74 @@ import styles from './styles.module.scss'
 
 export default function Home() {
   const [session, loading] = useSession()
+  const [fileData, setFileData] = useState({})
+  const [fileName, setFileName] = useState('')
+  const [fileSize, setFileSize] = useState(0)
+
+  const [progressTransfer, setProgressTransfer] = useState(0)
+
 
   const router = useRouter()
 
+  function handleChangeFile(event: ChangeEvent<HTMLInputElement>) {
+    setProgressTransfer(0)
+
+    let bucketName = 'data'
+    const userInfo = session?.user?.email
+
+
+
+    if (event.target.files) {
+      setFileData(event.target.files[0])
+
+      const storageRef2 = firebase.storage()
+
+      if (event.target.files[0]) {
+        let file = event.target.files[0]
+
+        const uploadTask = storageRef2.ref(`${bucketName}/${userInfo}/${file.name}`)
+          .put(file)
+
+        setFileSize(file.size)
+
+        uploadTask.on("state_changed", function (snapshot) {
+          let progress = (snapshot.bytesTransferred * 100 / snapshot.totalBytes)
+          let progressFormatted = progress.toFixed(0)
+          setProgressTransfer(Number(progressFormatted))
+
+        }, function (error) {
+          console.log(error)
+
+        }, function () {
+          setFileData({})
+          setProgressTransfer(0)
+          setFileSize(0)
+
+          console.log('Upload success')
+        })
+      }
+    }
+
+  }
+
+
+
+  useEffect(() => {
+    if (!fileData) {
+      setProgressTransfer(0)
+      setFileName('')
+      setFileSize(0)
+    }
+  }, [fileData])
+
+  useEffect(() => {
+    console.log(progressTransfer)
+  }, [progressTransfer])
+
+
   useEffect(() => {
     !session && router.push('/')
-  })
+  }, [session, router])
 
   return (
     <div className={styles.container}>
@@ -24,6 +88,21 @@ export default function Home() {
         <NavBar />
         <div className={styles.content}>
           <h1 className={styles.title}>Home</h1>
+
+
+          <div>
+            <input type="file" onChange={(event) => handleChangeFile(event)} />
+            <span>{fileSize > 0 && fileSize}</span>
+            <progress max="100" value={progressTransfer} />
+            <span>{fileSize > 0 && progressTransfer}</span>
+          </div>
+
+          <div>
+            <h5>Transferencia recente</h5>
+
+
+          </div>
+
         </div>
 
       </div>
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,15 +1,6 @@
 import firebase from 'firebase/app'
 
-import 'firebase/auth'
-
-// if (!firebase.apps.length) {
-//   firebase.initializeApp({
-//     apiKey: "AIzaSyD4gTk5gCVJBNFb8aW_74O-Wt5LM4JyVWw",
-//     authDomain: "loginandupload-79285.firebaseapp.com",
-//     projectId: "loginandupload-79285",
-//   })
-// }
-
+import 'firebase/storage'
 
 
 const firebaseConfig = {
@@ -20,24 +11,13 @@ const firebaseConfig = {
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_APP_ID
 }
+// Initialize Firebase
 
-firebase.initializeApp(firebaseConfig)
-
-/*
-
-
-firebase.initializeApp(firebaseConfig)
-// @ts-ignore
-const auth = firebase.auth()
-// @ts-ignore
-const database = firebase.database()
-
-
-export {
-  firebase,
-  auth, 
-  database 
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig)
+} else {
+  firebase.app(); // if already initialized, use that one
 }
 
-*/
 export default firebase
+
